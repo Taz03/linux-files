3186fe85d604ab340531d8cd34169664d9b8cb23
Tanish <tanishazad03@gmail.com>
2024-03-14 16:59:06 +0530

    Delete .config/nvim/lua/plugins.lua

--- .config/nvim/lua/plugins.lua
+++
@@ -1,9 +0,0 @@
-return {
-    { "williamboman/mason.nvim" },
-    { "williamboman/mason-lspconfig.nvim" },
-    { "VonHeikemen/lsp-zero.nvim" },
-    { "neovim/nvim-lspconfig" },
-    { "hrsh7th/cmp-nvim-lsp" },
-    { "hrsh7th/nvim-cmp" },
-    { "L3MON4D3/LuaSnip" }
-}
