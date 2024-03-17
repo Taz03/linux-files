241932f5ec7069ba18584fb441af7564cf473cf2
Tanish <tanishazad03@gmail.com>
2024-03-18 01:02:58 +0530

    Add files via upload

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -15,36 +15,7 @@ return {
             }
         end
     },
-    {
-        "HiPhish/rainbow-delimiters.nvim",
-        config = function()
-            local rainbow_delimiters = require "rainbow-delimiters"
-
-            require "rainbow-delimiters.setup".setup {
-                strategy = {
-                    [""] = rainbow_delimiters.strategy["global"],
-                    vim = rainbow_delimiters.strategy["local"],
-                },
-                query = {
-                    [""] = "rainbow-delimiters",
-                    lua = "rainbow-blocks",
-                },
-                priority = {
-                    [""] = 110,
-                    lua = 210,
-                },
-                highlight = {
-                    "RainbowDelimiterRed",
-                    "RainbowDelimiterYellow",
-                    "RainbowDelimiterBlue",
-                    "RainbowDelimiterOrange",
-                    "RainbowDelimiterGreen",
-                    "RainbowDelimiterViolet",
-                    "RainbowDelimiterCyan",
-                },
-            }
-        end
-    },
+    { "HiPhish/rainbow-delimiters.nvim" },
     {
         "lukas-reineke/indent-blankline.nvim",
         main = "ibl",
@@ -71,10 +42,7 @@ return {
                 vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
             end)
 
-            vim.g.rainbow_delimiters = { highlight = highlight }
-            require "ibl".setup { scope = { highlight = highlight } }
-
-            hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
+            require "ibl".setup { indent = { highlight = highlight } }
         end
     }
 }
