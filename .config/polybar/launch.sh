2cc17e48b4bb2c0f368154c2fb8aabbe0f2a8b68
Tanish Azad <tanishazad03@gmail.com>
2024-07-16 16:56:25 +0530

    update

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -3,3 +3,7 @@
 killall -q polybar
 
 polybar bar 2>&1 | tee -a /tmp/polybar.log & disown
+
+if [[ $(xrandr -q | grep 'HDMI-1-0 connected') ]]; then
+    polybar bar_external 2>&1 | tee -a /tmp/polybar.log & disown
+fi
